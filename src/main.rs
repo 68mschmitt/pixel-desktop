e2829b070862143e26e399db4e01725f42f11dfb
68mschmitt <68mschmitt@gmail.com>
2026-07-28 18:50:08 -0400

    simple main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+fn spend_energy(current_energy: i32, amount: i32) -> i32{
+    current_energy - amount
+}
+
 fn main() {
-    println!("Hello, world!");
+    let player_name = "Ada";
+    let mut energy = 10;
+
+    println!("{player_name}, start with {energy} energy");
+
+    energy = spend_energy(energy, 3);
+
+    println!("{player_name}, now has {energy} energy");
 }
